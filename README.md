2e0ac6d31267c14f3a2eeac2246f2aa058ed7df4
Marcus Lease <inbox@marcuslease.digital>
2019-05-11 19:19:16 +0200

    fixed 2nd dir message breaking; added pseudorandom sampling

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 a Max object to get random samplings of audio files from a directory
 
 ## todo
-* second dir message bombs
 * better randomize sampling
 * non-ascii chars in filenames breaks
 * additional error checking (nftw, etc.)
--- src/dirsampling.c
+++ src/dirsampling.c
@@ -1,21 +1,18 @@
 #define MAXAPI_USE_MSCRT
 #include "ext.h"
-#include "ext_obex.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <time.h>
 #include <dirent.h>
 #include <ftw.h>
-#include <errno.h>
 
 const int DEFAULT_NUM_SAMPLES = 99;
 
 /*
  * class pointer
  */
-void *dirsampling_class;
+static t_class *dirsampling_class;
 
 /*
  * instance struct
@@ -36,10 +33,9 @@ int dirsampling_nftw_callback(const char *,
                               struct FTW *);
 bool dirsampling_is_dir_accessible(const char *);
 bool dirsampling_is_file_audio(const char *);
-void dirsampling_send_hardcoded_samples(dirsampling *);
 
 /*
- * buffer to hold results of a dir walk
+ * buffer for audio files within a dir
  */
 static t_atomarray *dirsampling_buffer;
 
@@ -83,25 +79,37 @@ void dirsampling_free()
  */
 void dirsampling_dir(dirsampling *x, t_symbol *dir)
 {
+	if (!dirsampling_is_dir_accessible(dir->s_name)) {
+		object_error((t_object *)x, "dir not accessible");
+		return;
+	}	
+
 	dirsampling_buffer = atomarray_new(0, NULL);
+	long status;
+	status = nftw(dir->s_name, dirsampling_nftw_callback, 4, 0);
+	if (status != 0) goto error;
 
-	int err = nftw(dir->s_name, dirsampling_nftw_callback, 4, 0);
-	if (err != 0) perror("nftw() error");
-
-	for (int i = 0; i < DEFAULT_NUM_SAMPLES; i++) {
-		int min = 0, max = DEFAULT_NUM_SAMPLES - 1;
-		int r = min + rand() / (RAND_MAX / (max - min + 1) + 1);
-
-		t_atom *a;
-		atomarray_getindex(dirsampling_buffer, r, a);
-
-		t_atom argv[2];
-		atom_setlong(argv, i + 1);
-		atom_setsym(argv + 1, atom_getsym(a));
-		outlet_list(x->outlet1, NULL, 2, argv);
+	long len = atomarray_getsize(dirsampling_buffer);
+	int min = 0;
+	int max = len - 1;
+	srand(time(0));
+	for (int i = 0; i < DEFAULT_NUM_SAMPLES; i += 2) {
+		int buffer_i = min + rand() / (RAND_MAX / (max - min + 1) + 1);
+		t_atom cur[2];
+		atom_setlong(cur, i);
+		status = atomarray_getindex(dirsampling_buffer, buffer_i, cur + 1);
+		if (status != 0) goto error;
+		outlet_list(x->outlet1, NULL, 2, cur);
 	}
 
 	atomarray_clear(dirsampling_buffer);
+	return;
+error:
+	object_error(
+		(t_object *)x,
+		"unexpected exit status (%ld) in dirsampling_dir()",
+		status
+	);
 }
 
 /*
@@ -114,11 +122,16 @@ int dirsampling_nftw_callback(const char *file,
 {
 	if (i != FTW_F || !dirsampling_is_file_audio(file)) return 0;
 
-	t_atom *a;
-	atom_setsym(a, gensym(file));
-	atomarray_appendatom(dirsampling_buffer, a);
+	long status;
+	t_atom *a_file;
+
+	status = atom_setsym(a_file, gensym(file));
+	if (status != 0) goto error;
+	atomarray_appendatom(dirsampling_buffer, a_file);
 
 	return 0;
+error:
+	error("unexpected exit status (%ld) in dirsampling_nftw_callback()");
 }
 
 bool dirsampling_is_dir_accessible(const char *dir)
@@ -146,3 +159,5 @@ bool dirsampling_is_file_audio(const char *file)
 	else
 		return false;
 }
+
+
